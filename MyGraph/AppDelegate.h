53ce50735b1784df251c813f187c6dfd44efb387
Lucas Rosenblatt <lucasrosenblatt@Lucass-MacBook-Pro.local>
2013-09-30 13:59:31 -0400

    Initial Commit

---
+++ MyGraph/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  MyGraph
+//
+//  Created by Lucas Rosenblatt on 9/30/13.
+//  Copyright (c) 2013 Lucas Rosenblatt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ MyGraph/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MyGraph
+//
+//  Created by Lucas Rosenblatt on 9/30/13.
+//  Copyright (c) 2013 Lucas Rosenblatt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
